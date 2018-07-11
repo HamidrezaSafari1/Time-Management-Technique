53222b7636ad0a5dbc8720264f5e61f61ed284e7
safari <safari@Qazvin.tto>
2018-07-11 13:32:30 +0430

    two way is working again

--- src/Timer/components/TimerButton.jsx
+++ src/Timer/components/TimerButton.jsx
@@ -5,23 +5,23 @@ class TimerButton extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			currentTime: moment().hours(0).minutes(25).seconds(0),
+			currentTime: this.props.currentTime,
 			title: "Work",
-			cunter: 2
+			cunter: 4
 		};
 	}
 
 	start() {
 		new Promise((resolve, reject) => {
 			this.setState({ title: "Work" });
-			this.setState({ currentTime: moment().hours(0).minutes(10).seconds(0) });
+			this.setState({ currentTime: moment().hours(0).minutes(25).seconds(0) });
 			var interval = setInterval(() => {
 				this.setState({ currentTime: this.state.currentTime.subtract(1, "seconds") });
 				if ((this.state.currentTime.isBefore(moment({ hour: 0, minute: 0, second: 1 })))) {
 					clearInterval(interval);
 					return resolve();
 				}
-			}, 5);
+			}, 1000);
 		}).then(() => {
 			this.setState({ currentTime: moment().hours(0).minutes(5).seconds(0) });
 			this.setState({ title: "Rest" });
@@ -35,7 +35,7 @@ class TimerButton extends Component {
 					else if (this.state.cunter <= 0)
 						this.wait();
 				}
-			}, 10);
+			}, 1000);
 		});
 	}
 
@@ -47,7 +47,7 @@ class TimerButton extends Component {
 			this.setState({ currentTime: this.state.currentTime.subtract(1, "seconds") });
 			if ((this.state.currentTime.isBefore(moment({ hour: 0, minute: 0, second: 1 }))))
 				clearInterval(lastInterval);
-		}, 10);
+		}, 1000);
 	}
 
 
@@ -56,9 +56,6 @@ class TimerButton extends Component {
 		this.start();
 	}
 
-	titleChange() {
-		this.setState({ title: "Rest" });
-	}
 
 	render() {
 		return (
