d176112d9f258aea16fe6048b90538642e6a9f75
safari <safari@Qazvin.tto>
2018-07-12 12:14:39 +0430

    add image

--- src/Timer/components/TimerButton.jsx
+++ src/Timer/components/TimerButton.jsx
@@ -6,15 +6,17 @@ class TimerButton extends Component {
 		super(props);
 		this.state = {
 			currentTime: this.props.currentTime,
-			title: "Work",
+			state: "Work",
+			image_src: "./img/work.png",
 			cunter: 4
 		};
 	}
 
 	start() {
 		new Promise((resolve, reject) => {
-			this.setState({ title: "Work" });
-			this.setState({ currentTime: this.props.currentTime });
+			this.setState({ state: "Work" });
+			this.setState({ image_src: "./img/work.png" });
+			this.setState({ currentTime: moment().hours(0).minutes(25).seconds(0) });
 			var interval = setInterval(() => {
 				this.setState({ currentTime: this.state.currentTime.subtract(1, "seconds") });
 				if ((this.state.currentTime.isBefore(moment({ hour: 0, minute: 0, second: 1 })))) {
@@ -23,10 +25,12 @@ class TimerButton extends Component {
 				}
 			}, 10);
 		}).then(() => {
-			this.setState({ title: "Rest" });
+			this.setState({ state: "Rest" });
+			this.setState({ image_src: "./img/rest.png" });
 			this.setState({ cunter: this.state.cunter - 1 });
 			if (this.state.cunter <= 0) {
 				this.setState({ currentTime: moment().hours(0).minutes(30).seconds(0) });
+				clearInterval(restinterval);
 				this.wait();
 			} else {
 				this.setState({ currentTime: moment().hours(0).minutes(5).seconds(0) });
@@ -61,23 +65,30 @@ class TimerButton extends Component {
 	render() {
 		return (
 			<div>
-				<div>
-					{
-						this.state.title?10<20:this.state.cunter
-					}
+				<div class="container">
+					<div class="row justify-content-md-center">
+						<div class="col col-lg-2">
+							<img src={this.state.image_src} height="60" width="60" />
+						</div>
+					</div>
+					<div className="d-block mx-auto" >
+						<div className="row">
+							<h3 className="d-block mx-auto">{this.state.state}</h3>
+						</div>
+					</div>
 				</div>
-				<div>{this.state.title}</div>
-				<div>{this.state.cunter}</div>
-				<div className="row">
-					<h1 className="d-block mx-auto">
-						{this.state.currentTime.format("HH:mm:ss")}  </h1>
-				</div>
-				<div className="button">
-					<button className="btn btn-danger d-block mx-auto" onClick={this.handleStart}>Start</button>
-				</div>
-			</div>
-		);
-	}
-}
-export default TimerButton;
 
+					<div className="row">
+						<h1 className="d-block mx-auto">
+							{this.state.currentTime.format("HH:mm:ss")}  </h1>
+					</div>
+					<div className="button">
+						<button className="btn btn-danger d-block mx-auto" onClick={this.handleStart}>Start</button>
+					</div>
+					<hr />
+				</div>
+				);
+			}
+		}
+		export default TimerButton;
+		
